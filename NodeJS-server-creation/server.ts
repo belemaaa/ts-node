35687b470843eb0fb9029b41d842d38f19935d2c
belemaaa <graceitamunoala@gmail.com>
2024-10-01 16:46:24 +0100

    file system - reading and writing to a file

--- NodeJS-server-creation/server.ts
+++ NodeJS-server-creation/server.ts
@@ -1,5 +1,7 @@
 import http, {Server, IncomingMessage, ServerResponse} from 'http'
 import os from 'os'
+import fs from 'fs'
+import path from 'path'
 
 const hostname: string = '127.0.0.1'
 const port: number = 5000
@@ -13,8 +15,22 @@ const server: Server = http.createServer(( request: IncomingMessage, response: S
         totalMemory: os.totalmem(),
         freeMemory: os.freemem(),
         homedir: os.homedir(),
-        computerName: os.hostname()
+        computerName: os.hostname(),
+        platform: os.platform()
     }
+
+    // fs module
+    fs.readFile(path.join(__dirname, 'data', 'notes.txt'), 'utf-8', (error, result) => {
+        if(error){
+            throw(error)
+        }
+        fs.writeFile(path.join(__dirname, 'data', 'data.txt'), result, 'utf-8', (error) => {
+            if(error){
+                throw error
+            }
+        })
+        response.end(`${result}`)
+    })
 
     //response.end("Welcome to nodejs server</h3>")
     response.end(`${JSON.stringify(osData)}`)
