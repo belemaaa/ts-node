f49f15b74c55a041b2087170c0c62901e290aea2
belemaaa <graceitamunoala@gmail.com>
2024-10-05 22:52:09 +0100

    mongodb connection

--- API/src/index.ts
+++ API/src/index.ts
@@ -5,6 +5,10 @@ import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
 import { Server } from 'http'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -24,3 +28,12 @@ const hostname: string = "http://localhost"
 server.listen(8080, () => {
     console.log(`Server is running on ${hostname}:${port}/`)
 })
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME || 'defaultUsername'}:${process.env.MONGO_PASSWORD || 'defaultPassword'}@${process.env.MONGO_CLUSTER || 'defaultCluster'}/?retryWrites=true&w=majority`;
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('MongoDB connection error:', error));
+
+console.log(MONGO_URL)
