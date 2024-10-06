b6d6e612cf3cd65aef169ce271f5739f1b2a139d
belemaaa <graceitamunoala@gmail.com>
2024-10-06 12:39:25 +0100

    added login route

--- API/src/router/authentication.ts
+++ API/src/router/authentication.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express';
-import { register } from '../controllers/authentication';
+import { register, login } from '../controllers/authentication';
 
 const authenticationRouter: Router = express.Router();
 
 authenticationRouter.post('/auth/register', register as any);
+authenticationRouter.post('/auth/login', login as any)
 
 export default authenticationRouter; 
