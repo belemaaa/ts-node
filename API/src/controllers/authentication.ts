c15b84a68b289c8efee0f496c1d18be07ef40996
belemaaa <graceitamunoala@gmail.com>
2024-10-06 12:31:59 +0100

    login controller

--- API/src/controllers/authentication.ts
+++ API/src/controllers/authentication.ts
@@ -32,3 +32,37 @@ export const register = async (req: express.Request, res: express.Response): Pro
         return res.status(400).json({ error: error})
     }
 }
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try{
+        const {email, password} = req.body
+        if (!email || !password){
+            return res.status(400).json({ error: "Invalid credentials"})
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+        if (!user){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+
+        const expectedHash = authentication(user.authentication?.salt || '', password)
+        if (user.authentication?.password !== expectedHash){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+        
+        //configure session token for the user
+        const salt = generateSalt()
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+        await user.save()
+
+        res.cookie('AUTH', user.authentication.sessionToken, {
+            domain: 'localhost',
+            path: '/'
+        })
+        return res.status(200).json(user).end()
+
+    } catch (error){
+        console.log(error)
+        return res.status(400).json({ error: error })
+    }
+}
